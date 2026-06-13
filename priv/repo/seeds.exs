28ebb5dfe3b0911fd9b26f11a6cf0829e0c03e32
Tan Kwang How <tankwanghow@gmail.com>
2026-06-14 06:39:17 +0800

    chore: seed system obligation type presets

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,44 @@
 # Script for populating the database. You can run it as:
 #
 #     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Argus.Repo.insert!(%Argus.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+
+alias Argus.Obligations.Type
+alias Argus.Repo
+
+presets = [
+  %{
+    name: "EPF Monthly",
+    recurring_interval: "monthly",
+    complete_documents: "payment_receipt",
+    reminder_offsets: "7,1"
+  },
+  %{
+    name: "SOCSO Monthly",
+    recurring_interval: "monthly",
+    reminder_offsets: "7,1"
+  },
+  %{
+    name: "SST Return",
+    recurring_interval: "quarterly",
+    reminder_offsets: "30,7,1"
+  },
+  %{
+    name: "SSM Annual Return",
+    recurring_interval: "annual",
+    reminder_offsets: "30,7,1"
+  },
+  %{
+    name: "LHDN Tax Estimation",
+    recurring_interval: "custom",
+    reminder_offsets: "30,7,1"
+  }
+]
+
+for attrs <- presets do
+  %Type{entity_id: nil}
+  |> Type.changeset(attrs)
+  |> Repo.insert!(
+    on_conflict: :nothing,
+    conflict_target: [:entity_id, :name]
+  )
+end
